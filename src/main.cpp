0b70e9a2d805519a69f4abffbfbee85612ed90a5
Szikaka <251594@edu.p.lodz.pl>
2025-11-21 10:04:09 +0100

    Added OpenGL debug output

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,51 @@ static void GLFWErrorCallback(int error, const char* description) {
 	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
+static void APIENTRY glDebugOutput(
+	GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char *message,
+	const void *userParam
+) {
+	// ignore non-significant error/warning codes
+	if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::string sourceString;
+
+	switch (source) {
+		case GL_DEBUG_SOURCE_API:             sourceString = "API"; break;
+		case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   sourceString = "Window System"; break;
+		case GL_DEBUG_SOURCE_SHADER_COMPILER: sourceString = "Shader Compiler"; break;
+		case GL_DEBUG_SOURCE_THIRD_PARTY:     sourceString = "Third Party"; break;
+		case GL_DEBUG_SOURCE_APPLICATION:     sourceString = "Application"; break;
+		case GL_DEBUG_SOURCE_OTHER:           sourceString = "Other"; break;
+	}
+
+	std::string typeString;
+
+	switch (type) {
+		case GL_DEBUG_TYPE_ERROR:               typeString = "Error"; break;
+		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: typeString = "Deprecated Behaviour"; break;
+		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  typeString = "Undefined Behaviour"; break; 
+		case GL_DEBUG_TYPE_PORTABILITY:         typeString = "Portability"; break;
+		case GL_DEBUG_TYPE_PERFORMANCE:         typeString = "Performance"; break;
+		case GL_DEBUG_TYPE_MARKER:              typeString = "Marker"; break;
+		case GL_DEBUG_TYPE_PUSH_GROUP:          typeString = "Push Group"; break;
+		case GL_DEBUG_TYPE_POP_GROUP:           typeString = "Pop Group"; break;
+		case GL_DEBUG_TYPE_OTHER:               typeString = "Other"; break;
+	}
+
+	switch (severity) {
+		case GL_DEBUG_SEVERITY_HIGH:         spdlog::error("GL {} {}: {} ({})", sourceString, typeString, message, id); break;
+		case GL_DEBUG_SEVERITY_MEDIUM:
+		case GL_DEBUG_SEVERITY_LOW:          spdlog::warn("GL {} {}: {} ({})", sourceString, typeString, message, id); break;
+		case GL_DEBUG_SEVERITY_NOTIFICATION: spdlog::info("GL {} {}: {} ({})", sourceString, typeString, message, id); break;
+	}
+}
+
 bool InitProgram();
 void InitImgui();
 
@@ -251,7 +296,9 @@ bool InitProgram() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
 	glfwWindowHint(GLFW_OPENGL_PROFILE,        GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT,  true);
 
 	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GLGP Project", NULL, NULL);
 	if (window == NULL) {
@@ -263,6 +310,16 @@ bool InitProgram() {
 	glfwSwapInterval(1);
 
 	bool err = !gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	
+	int contextFlags = 0;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &contextFlags);
+
+	if (contextFlags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, true);
+	}
 	
 	glEnable(GL_DEPTH_TEST);
 	
