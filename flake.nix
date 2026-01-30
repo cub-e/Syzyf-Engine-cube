ee9664dcd7c01e055d59be18f33af45a9dac04f4
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-01-30 18:29:27 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Syzyf Engine - C++, OpenGL";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      runtimeLibs = with pkgs; [
+        libGL
+        libGLU
+
+        wayland
+        libxkbcommon
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXext
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+          pkg-config
+          gdb
+          wayland-scanner
+        ];
+
+        buildInputs = with pkgs; [
+          zlib
+          libffi
+        ] ++ runtimeLibs;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH
+        '';
+      };
+    };
+}
